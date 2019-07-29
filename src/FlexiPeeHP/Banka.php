11a4b6427fe2e631fdfdd280258dca0fa3140693
Vítězslav Dvořák <info@vitexsoftware.cz>
2019-07-29 14:23:46 +0200

    Add the filter param to the automatickeParovani method

--- src/FlexiPeeHP/Banka.php
+++ src/FlexiPeeHP/Banka.php
@@ -38,18 +38,20 @@ class Banka extends FlexiBeeRW
     }
 
     /**
-     * Start invoice authomatic matching process ( it take longer time )
+     * Start invoice automatic matching process ( it takes longer time )
      * Spustí proces automatického párování plateb. ( trvá delší dobu )
      *
      * @link https://demo.flexibee.eu/devdoc/parovani-plateb Interní dokumentace
      * 
      * @param boolean $advanced Use Advanced matching method ?
+     * @param string $filter Filter bank records before pairing ?
      * 
      * @return boolean
      */
-    public function automatickeParovani($advanced = false)
+    public function automatickeParovani($advanced = false, $filter = null)
     {
-        $this->performRequest('automaticke-parovani'.($advanced ? '-pokrocile' : '' ), 'PUT');
+        $filterUrl = $filter === null ? "" : rtrim($filter, '/') . '/';
+        $this->performRequest($filterUrl . 'automaticke-parovani'.($advanced ? '-pokrocile' : '' ), 'PUT');
         return $this->lastResponseCode == 200;
     }
 }
