58fe0d1205745a087e7745f1123a3e75afc7c6c4
Petr Balvín <petr.balvin@haxo.cz>
2020-02-27 13:20:00 +0100

    ObjednavkaPrijataPolozka.php

---
+++ src/FlexiPeeHP/ObjednavkaPrijataPolozka.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace FlexiPeeHP;
+
+/**
+ * Položka skladového pohybu
+ *
+ * @link https://demo.flexibee.eu/c/demo/objednavka-prijata-polozka/properties
+ */
+class ObjednavkaPrijataPolozka extends FlexiBeeRW
+{
+
+    /**
+     * Evidence užitá objektem.
+     *
+     * @var string
+     */
+    public $evidence = 'objednavka-prijata-polozka';
+
+
+}
