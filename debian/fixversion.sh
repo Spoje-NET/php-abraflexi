df043d0dd2a6cb6f14caa194d0149dc395284699
Vítězslav Dvořák <info@vitexsoftware.cz>
2020-10-18 23:10:37 +0200

    Release v1.37

--- debian/fixversion.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-VERSTR=`dpkg-parsechangelog --show-field Version`
-COMPOSER_VERSTR=`echo ${VERSTR}|sed 's/-/./g'`
-sed -i -e "/public static \$libVersion/c\    public static \$libVersion = '${VERSTR}-deb';" debian/php-spojenet-flexibee/usr/share/php/FlexiPeeHP/FlexiBeeRO.php
-sed -i -e "/public static \$libVersion/c\    public static \$libVersion = '${VERSTR}';" src/FlexiPeeHP/FlexiBeeRO.php
