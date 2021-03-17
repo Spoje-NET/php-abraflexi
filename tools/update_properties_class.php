2b8fc0195142f12f08fbe7ed292cb5a606c1ad5b
Vítězslav Dvořák <info@vitexsoftware.cz>
2021-03-17 17:25:52 +0100

    Missing column names added to 'nastaveni' structure array

--- tools/update_properties_class.php
+++ tools/update_properties_class.php
@@ -7,19 +7,19 @@ define('EASE_LOGGER', 'console|syslog');
 
 require_once '../testing/bootstrap.php';
 
-function evidenceToClass($evidence)
-{
+function evidenceToClass($evidence) {
     return str_replace(' ', '', ucwords(str_replace('-', ' ', $evidence)));
 }
+
 $outFile = 'Properties.php';
-$ok      = 0;
+$ok = 0;
 
 $evidenceProps = '<?php
 /**
  * AbraFlexi - Evidence Properties.
  *
  * @author     Vítězslav Dvořák <info@vitexsoftware.cz>
- * @copyright  (C) 2015-'.date('Y').' Spoje.Net
+ * @copyright  (C) 2015-' . date('Y') . ' Spoje.Net
  */
 
 namespace AbraFlexi;
@@ -36,7 +36,6 @@ class Properties
 
 $statuser = new Status();
 
-
 $syncer = new RO();
 $syncer->setObjectName('FlexiBee Evidence Properties');
 $syncer->addStatusMessage('Updating Evidences Properties');
@@ -45,44 +44,44 @@ $pos = 0;
 foreach (EvidenceList::$name as $evidencePath => $evidenceName) {
     $pos++;
     if ($evidencePath == 'nastaveni') {
-        $info      = json_decode(file_get_contents('nastaveni-properties.json'),
-            true);
-        $structure = $info['properties']['property'];
+        $info = json_decode(file_get_contents('nastaveni-properties.json'),
+                true);
+        $structure = \Ease\Functions::reindexArrayBy($info['properties']['property'], 'propertyName');
     } else {
         $structure = $syncer->getOnlineColumnsInfo($evidencePath);
     }
 
     if (count($structure)) {
-        $syncer->addStatusMessage($pos.' of '.count(EvidenceList::$name).' '.$evidencePath.': structure obtained',
-            'success');
+        $syncer->addStatusMessage($pos . ' of ' . count(EvidenceList::$name) . ' ' . $evidencePath . ': structure obtained',
+                'success');
         $ok++;
     } else {
-        $syncer->addStatusMessage($pos.' of '.count(EvidenceList::$name).' '.$evidencePath.': structure problem',
-            'error');
+        $syncer->addStatusMessage($pos . ' of ' . count(EvidenceList::$name) . ' ' . $evidencePath . ': structure problem',
+                'error');
     }
-    file_put_contents('Properties.'.$evidencePath.'.json',
-        json_encode($structure));
+    file_put_contents('Properties.' . $evidencePath . '.json',
+            json_encode($structure));
 
-    $evidenceClass     = evidenceToClass($evidencePath);
-    $evidenceClassFile = '../src/AbraFlexi/'.$evidenceClass.'.php';
+    $evidenceClass = evidenceToClass($evidencePath);
+    $evidenceClassFile = '../src/AbraFlexi/' . $evidenceClass . '.php';
     if (file_exists($evidenceClassFile)) {
         $evidenceClassBody = file_get_contents($evidenceClassFile);
 
         if (array_key_exists('firma', $structure)) {
             if (!strstr($evidenceClassBody, 'use Firma;')) {
                 $syncer->addStatusMessage(sprintf('use Firma; not found in %s ',
-                        $evidenceClassFile), 'warning');
+                                $evidenceClassFile), 'warning');
             }
         }
 
         if (array_key_exists('stitky', $structure)) {
             if (!strstr($evidenceClassBody, 'use Stitky;')) {
                 $syncer->addStatusMessage(sprintf('use Stitky; not found in %s ',
-                        $evidenceClassFile), 'warning');
+                                $evidenceClassFile), 'warning');
             }
         }
     }
 }
 
-$syncer->addStatusMessage('Updating of '.$ok.' Evidences Properties done',
-    'success');
+$syncer->addStatusMessage('Updating of ' . $ok . ' Evidences Properties done',
+        'success');
