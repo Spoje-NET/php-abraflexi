b9a1bac1f5f3330378e6e781a0a061ff4fab9f60
CyberVitexus <info@vitexsoftware.cz>
2023-06-03 23:36:14 +0200

    #33 - Dry Run suport for Sync

--- Examples/DryRun.php
+++ Examples/DryRun.php
@@ -4,19 +4,36 @@
  * AbraFlexi - Example how to dry-run request
  *
  * @author     Vítězslav Dvořák <info@vitexsofware.cz>
- * @copyright  (G) 2018 Vitex Software
+ * @copyright  (G) 2018-2023 Vitex Software
  */
 
 namespace Example\AbraFlexi;
 
 include_once './config.php';
-include_once '../vendor/autoload.php';
 
+/**
+ * Instance of AbraFlexi Invoice in dry-run mode with random code
+ */
 
-$invoice = new \AbraFlexi\FakturaVydana();
+$invoice = new \AbraFlexi\FakturaVydana([
+    'kod'=>'DRY_RUN_TEST', 
+    'typDokl' => 'code:FAKTURA',
+    'poznam' => 'Test ' . time(), 
+    'varSym' => time()], ['dry-run' => true]);
 
-$invoice->defaultUrlParams['dry-run'] = 'true';
+/**
+ * Save invoice to AbraFlexi
+ */
+ try {
+     $resultInsert = $invoice->insertToAbraFlexi();
+ } catch (\AbraFlexi\Exception $exc) {
+     echo $exc->getMessage();
+    //For example we can get here: "AbraFlexi\FakturaVydana: Pole 'Variabilní symbol' musí být vyplněno. [DRY_RUN_TEST]"
+ }
 
-$resultHere = $invoice->insertToFlexiBee(['typDokl' => 'code:FAKTURA']);
+/**
+ * Save to AbraFlexi and load result
+ */
+$resultSync = $invoice->sync();
 
-echo json_encode($resultHere, JSON_PRETTY_PRINT);;
+echo $resultSync ? 'OK' : 'Failed';
