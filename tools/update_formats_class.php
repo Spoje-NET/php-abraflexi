c07977be7c4249466d69c1b7bd17923cea7e2be0
CyberVitexus <info@vitexsoftware.cz>
2023-11-30 00:50:17 +0100

    JasperReport filetype added

--- tools/update_formats_class.php
+++ tools/update_formats_class.php
@@ -76,43 +76,114 @@ $evidenceFormats .= '
      * @see https://www.flexibee.eu/api/dokumentace/ref/format-types/
      * @var array formats known to flexibee
      */
-    static public $formats = [
-        \'JS\' => [\'desc\' => \'JavaScropt\',
-            \'suffix\' => \'js\', \'content-type\' => \'application/javascript\', \'import\' => false],
-        \'PNG\' => [\'desc\' => \'Portable Network Graphics\',
-            \'suffix\' => \'png\', \'content-type\' => \'image/png\', \'import\' => false],
-        \'CSS\' => [\'desc\' => \'Kaskádový styl\',
-            \'suffix\' => \'css\', \'content-type\' => \'text/css\', \'import\' => false],
-        \'HTML\' => [\'desc\' => \'HTML stránka pro zobrazení informací na webové stránce.\',
-            \'suffix\' => \'html\', \'content-type\' => \'text/html\', \'import\' => false],
-        \'XML\' => [\'desc\' => \'Strojově čitelná struktura ve formátu XML.\', \'suffix\' => \'xml\',
-            \'content-type\' => \'application/xml\', \'import\' => true],
-        \'JSON\' => [\'desc\' => \'Strojově čitelná struktura ve formátu JSON. \', \'suffix\' => \'json\',
-            \'content-type\' => \'application/json\', \'import\' => true],
-        \'CSV\' => [\'desc\' => \'Tabulkový výstup do formátu CSV (Column Separated Values).\',
-            \'suffix\' => \'csv\', \'content-type\' => \'text/csv\', \'import\' => true],
-        \'DBF\' => [\'desc\' => \'Databázový výstup ve formátu DBF (dBase).\', \'suffix\' => \'dbf\',
-            \'content-type\' => \'application/dbf\', \'import\' => true],
-        \'XLS\' => [\'desc\' => \'Tabulkový výstup ve formátu Excel.\', \'suffix\' => \'xls\',
-            \'content-type\' => \'application/ms-excel\', \'import\' => true],
-        \'ISDOC\' => [\'desc\' => \'e-faktura ISDOC.\', \'suffix\' => \'isdoc\', \'content-type\' => \'application/x-isdoc\',
-            \'import\' => false],
-        \'ISDOCx\' => [\'desc\' => \'e-faktura ISDOC s PDF přílohou\', \'suffix\' => \'isdocx\',
+    public static $formats = [
+        \'JS\' => [
+            \'desc\' => \'JavaScript\',
+            \'suffix\' => \'js\', 
+            \'content-type\' => \'application/javascript\', 
+            \'import\' => false
+            ],
+        \'PNG\' => [
+            \'desc\' => \'Portable Network Graphics\',
+            \'suffix\' => \'png\', 
+            \'content-type\' => \'image/png\', 
+            \'import\' => false
+            ],
+        \'CSS\' => [
+            \'desc\' => \'Kaskádový styl\',
+            \'suffix\' => \'css\', 
+            \'content-type\' => \'text/css\', 
+            \'import\' => false
+            ],
+        \'HTML\' => [
+            \'desc\' => \'HTML stránka pro zobrazení informací na webové stránce.\',
+            \'suffix\' => \'html\', 
+            \'content-type\' => \'text/html\', 
+            \'import\' => false
+            ],
+        \'XML\' => [
+            \'desc\' => \'Strojově čitelná struktura ve formátu XML.\', 
+            \'suffix\' => \'xml\',
+            \'content-type\' => \'application/xml\', 
+            \'import\' => true],
+        \'JSON\' => [
+            \'desc\' => \'Strojově čitelná struktura ve formátu JSON. \', 
+            \'suffix\' => \'json\',
+            \'content-type\' => \'application/json\', 
+            \'import\' => true
+            ],
+        \'CSV\' => [
+            \'desc\' => \'Tabulkový výstup do formátu CSV (Column Separated Values).\',
+            \'suffix\' => \'csv\', 
+            \'content-type\' => \'text/csv\', 
+            \'import\' => true
+            ],
+        \'DBF\' => [
+            \'desc\' => \'Databázový výstup ve formátu DBF (dBase).\', 
+            \'suffix\' => \'dbf\',
+            \'content-type\' => \'application/dbf\', 
+            \'import\' => true
+            ],
+        \'XLS\' => [
+            \'desc\' => \'Tabulkový výstup ve formátu Excel.\', 
+            \'suffix\' => \'xls\',
+            \'content-type\' => \'application/ms-excel\', 
+            \'import\' => true
+            ],
+        \'ISDOC\' => [
+            \'desc\' => \'e-faktura ISDOC.\', 
+            \'suffix\' => \'isdoc\', 
+            \'content-type\' => \'application/x-isdoc\',
+            \'import\' => false
+            ],
+        \'ISDOCx\' => [
+            \'desc\' => \'e-faktura ISDOC s PDF přílohou\', 
+            \'suffix\' => \'isdocx\',
             \'content-type\' => \'application/x-isdocx\',
-            \'import\' => false],
-        \'EDI\' => [\'desc\' => \'Elektronická výměna data (EDI) ve formátu INHOUSE.\',
-            \'suffix\' => \'edi\', \'content-type\' => \'application/x-edi-inhouse\', \'import\' => \'objednavka-prijata\'],
-        \'PDF\' => [\'desc\' => \'Generování tiskového reportu. Jedná se o stejnou funkci která je dostupná v aplikaci. Export do PDF\',
-            \'suffix\' => \'pdf\', \'content-type\' => \'application/pdf\', \'import\' => false],
-        \'vCard\' => [\'desc\' => \'Výstup adresáře do formátu elektronické vizitky vCard.\',
-            \'suffix\' => \'vcf\', \'content-type\' => \'text/vcard\', \'import\' => false],
-        \'iCalendar\' => [\'desc\' => \'Výstup do kalendáře ve formátu iCalendar. Lze takto exportovat události, ale také třeba splatnosti u přijatých či vydaných faktur.\',
-            \'suffix\' => \'ical\', \'content-type\' => \'text/calendar\', \'import\' => false],
-        \'TEXT\' => [\'desc\' => \'Prostý text\', \'suffix\' => \'txt\',
-            \'content-type\' => \'text/plain\', \'import\' => false],
-        \'TTF\' => [\'desc\' => \'TrueType font\', \'suffix\' => \'ttf\',
-            \'content-type\' => \'application/x-font-truetype\', \'import\' => false],
-
+            \'import\' => false
+            ],
+        \'EDI\' => [
+            \'desc\' => \'Elektronická výměna data (EDI) ve formátu INHOUSE.\',
+            \'suffix\' => \'edi\', 
+            \'content-type\' => \'application/x-edi-inhouse\', 
+            \'import\' => \'objednavka-prijata\'
+            ],
+        \'PDF\' => [
+            \'desc\' => \'Generování tiskového reportu. Jedná se o stejnou funkci která je dostupná v aplikaci. Export do PDF\',
+            \'suffix\' => \'pdf\', 
+            \'content-type\' => \'application/pdf\', 
+            \'import\' => false
+            ],
+        \'vCard\' => [
+            \'desc\' => \'Výstup adresáře do formátu elektronické vizitky vCard.\',
+            \'suffix\' => \'vcf\', 
+            \'content-type\' => \'text/vcard\', 
+            \'import\' => false
+            ],
+        \'iCalendar\' => [
+            \'desc\' => \'Výstup do kalendáře ve formátu iCalendar. Lze takto exportovat události, ale také třeba splatnosti u přijatých či vydaných faktur.\',
+            \'suffix\' => \'ical\', 
+            \'content-type\' => \'text/calendar\', 
+            \'import\' => false
+            ],
+        \'TEXT\' => [
+            \'desc\' => \'Prostý text\', 
+            \'suffix\' => \'txt\',
+            \'content-type\' => \'text/plain\', 
+            \'import\' => false
+            ],
+        \'TTF\' => [
+            \'desc\' => \'TrueType font\', 
+            \'suffix\' => \'ttf\',
+            \'content-type\' => \'application/x-font-truetype\', 
+            \'import\' => false
+            ],
+        \'JASPERReport\' => [
+            \'desc\' => \'JasperReports\',
+            \'content-type\' => \'application/x-java-serialized-object\',
+            \'suffix\'=> \'jasper\',
+            \'import\'=> false
+        ]
     ];
 
 
