2dd021a3421f8663cd15d87090ccbfcce7f30da8
Jenkins <jenkins@vyvojar.spoje.net>
2024-07-04 09:50:59 +0200

    AbraFlexi release v2024.4.5

--- src/AbraFlexi/Actions.php
+++ src/AbraFlexi/Actions.php
@@ -22,7 +22,7 @@ class Actions
      *
      * @var string
      */
- static public $version = '2024.4.4';
+ static public $version = '2024.4.5';
 
     /**
      * Evidence adresar (Adresy firem) Actions.
