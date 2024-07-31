fbe02ea7e2e80e81a17f5c3eb57b13d707fabf78
SimonFormanek <mail@simonformanek.cz>
2024-07-31 02:33:24 +0200

    new cvlass udalost

---
+++ src/AbraFlexi/Udalost.php
@@ -0,0 +1,13 @@
+    <?php
+    /**
+     * @link https://demo.abraflexi.eu/c/demo/udalost/properties api udalost evidence
+     */
+    class Udalost extends /AbraFlexi/RW
+    {
+        /**
+         * Evidence užitá objektem.
+         *
+         * @var string
+         */
+        public $evidence = 'udalost';
+    }
